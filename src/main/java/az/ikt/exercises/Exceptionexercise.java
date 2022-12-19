636c3d10d2c05502b1bf973b3328e8766cfb8bae
LogmanovaSN <shahana.logmanova@kapitalbank.az>
2022-12-19 11:15:38 +0400

    Exception exercise updated

--- src/main/java/az/ikt/exercises/Exceptionexercise.java
+++ src/main/java/az/ikt/exercises/Exceptionexercise.java
@@ -6,36 +6,41 @@ import java.util.Scanner;
 public class Exceptionexercise {
     public static void main(String[] args) {
 
-        while (true) {
+        int x = method();
+        System.out.println(x);
+    }
+
+    private static int method() {
+
+        try {
             Scanner input = new Scanner(System.in);
+            System.out.println("Please enter number1");
+            int number1 = input.nextInt();
+            System.out.println("Please enter number2");
 
-            try {
-                System.out.println("Please enter number1");
-                int number1 = input.nextInt();
-                System.out.println("Please enter number2");
-
-                int number2 = input.nextInt();
-
-                System.out.println(number1 / number2);
-            } catch (ArithmeticException | InputMismatchException ex) {
-                if (ex instanceof ArithmeticException) {
-                    System.out.println("Sifira bolme mumkun deyil");
-                    break;
-                } else if (ex instanceof InputMismatchException) {
-                    System.out.println("Number is not integer type");
-                    break;
-                }
-            } finally {
-                System.out.println("Gelib sona catdiq");
-            }
-
+            int number2 = input.nextInt();
+            System.out.println("RESULT " + number1 / number2);
+            return number1 / number2;
+        } catch (ArithmeticException | InputMismatchException ex) {
+            if (ex instanceof ArithmeticException) {
+                System.out.println("Sifira bolme mumkun deyil");
+                return method();
+            } else if (ex instanceof InputMismatchException) {
+                System.out.println("Number is not integer type");
+                // return method();
+            } return method();
+        } finally {
+            System.out.println("Gelib sona catdiq");
 
         }
 
-
+        // return x}
     }
 
-
 }
+
+
+
+
 
 
