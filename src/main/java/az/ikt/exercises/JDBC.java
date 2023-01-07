8d4ddf2dffc111e12c7041040b9c7862b73c3148
Shahanalova <Sahan1993!>
2023-01-07 16:00:29 +0400

    jdbc

---
+++ src/main/java/az/ikt/exercises/JDBC.java
@@ -0,0 +1,130 @@
+package az.ikt.exercises;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBC {
+package az.iktlab.lessons.lesson29.smallapp.repository.impl;
+
+import az.iktlab.lessons.lesson29.smallapp.model.Gender;
+import az.iktlab.lessons.lesson29.smallapp.model.Person;
+import az.iktlab.lessons.lesson29.smallapp.model.Sql;
+import az.iktlab.lessons.lesson29.smallapp.repository.PersonRepository;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+    public class PersonRepoJdbcImpl implements PersonRepository {
+
+        Connection con;
+
+
+        private final Statement stmt;
+
+
+        public PersonRepoJdbcImpl() {
+            try {
+                Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+                        "postgres",
+                        "root");
+                this.stmt = con.createStatement();
+            } catch (SQLException e) {
+                throw new RuntimeException();
+            }
+        }
+
+        @Override
+        public Long findMaxId() {
+
+            try {
+                ResultSet rs = stmt.executeQuery(Sql.FIND_MAX_ID.getValue());
+
+                return rs.getLong("max_id");
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return 0L;
+            }
+        }
+
+        @Override
+        public void save(Person person) {
+
+            try {
+                stmt.execute("insert into person(id, name,surname,fathername,age, gender) values" + "(" + person.getId() + "," + person.getName() + "," + person.getSurname() + "," + person.getFatherName() + "," + person.getAge() + "," + person.getGender() + ");");
+
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        @Override
+        public List<Person> getAll() {
+            try {
+                ResultSet rs = stmt.executeQuery("select id,name, surname,fathername,age, gender from person;");
+                List<Person> personList = new ArrayList<>();
+
+                while (rs.next()) {
+                    Person person = new Person();
+                    person.setId(rs.getLong("id"));
+                    person.setName(rs.getString("name"));
+                    person.setSurname(rs.getString("surname"));
+                    person.setFatherName(rs.getString("fathername"));
+                    person.setAge(rs.getInt("age"));
+                    person.setGender(Gender.valueOf(rs.getString("gender")));
+                    personList.add(person);
+
+                }
+                return personList;
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return new ArrayList<Person>() {
+                };
+            }
+        }
+
+
+        @Override
+        public Person getById(Long aLong) {
+            try {
+                ResultSet rs = stmt.executeQuery("select id,name, surname,fathername,age, gender from person where id = " + aLong +";");
+                Person person = new Person();
+                person.setId(rs.getLong("id"));
+                person.setName(rs.getString("name"));
+                person.setSurname(rs.getString("surname"));
+                person.setFatherName(rs.getString("fathername"));
+                person.setAge(rs.getInt("age"));
+                person.setGender(Gender.valueOf(rs.getString("gender")));
+
+
+                return person;
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        public void delete(Person person) {
+            try {
+                stmt.execute("delete from person;");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void deleteById(Long aLong) {
+            try {
+                stmt.execute("delete from person where id = "+ aLong +";");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
